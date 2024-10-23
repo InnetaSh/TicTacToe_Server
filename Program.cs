dc131ad71f992890444b2f99af4ffbf6d36bd7a2
InnetaSh <inneta3@gmail.com>
2024-10-23 18:42:57 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,132 @@
+﻿
+
+
+
+using System.Net;
+using System.Net.Sockets;
+
+
+ServerObject listener = new ServerObject();
+await listener.ListenAsync();
+
+class ServerObject
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 13000);
+    List<ClientObject> clients = new List<ClientObject>();
+
+
+    protected internal async Task ListenAsync()
+    {
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+
+                ClientObject clientObject = new ClientObject(tcpClient, this);
+                clients.Add(clientObject);
+                Task.Run(clientObject.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+
+    protected internal async Task MessageAsync(string message, string id)
+    {
+        foreach (var client in clients)
+        {
+            if (client.Id != id)
+            {
+                await client.Writer.WriteLineAsync(message);
+                await client.Writer.FlushAsync();
+            }
+        }
+    }
+
+    protected internal void Disconnect()
+    {
+        foreach (var client in clients)
+        {
+            client.Close();
+        }
+        tcpListener.Stop();
+    }
+}
+
+
+
+
+class ClientObject
+{
+    protected internal string Id { get; } = Guid.NewGuid().ToString();
+    protected internal StreamWriter Writer { get; }
+    protected internal StreamReader Reader { get; }
+
+    TcpClient client;
+    ServerObject server;
+
+    public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+    {
+        client = tcpClient;
+        server = serverObject;
+
+        var stream = client.GetStream();
+
+        Reader = new StreamReader(stream);
+
+        Writer = new StreamWriter(stream);
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+
+            string? userName = await Reader.ReadLineAsync();
+            string? message = $"{userName} вошел в чат";
+
+            await server.MessageAsync(message, Id);
+            Console.WriteLine(message);
+
+            while (true)
+            {
+                try
+                {
+                    message = await Reader.ReadLineAsync();
+                    if (message == null) continue;
+                    message = $"{userName}: {message}";
+                    Console.WriteLine(message);
+                    await server.MessageAsync(message, Id);
+                }
+                catch
+                {
+                    message = $"{userName} покинул чат";
+                    Console.WriteLine(message);
+                    await server.MessageAsync(message, Id);
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        client.Close();
+    }
+}
